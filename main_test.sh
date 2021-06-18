424b0c436a1aa2e4fa88219bc05fe185a3f343df
psybrat <psybrat+git@ya.ru>
2021-06-18 20:28:30 +0300

    Terraform HighlyAvailable server config v.3 Correct install command, create easy index.html file

--- main_test.sh
+++ main_test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt update
-apt install apache2
+apt install -y apache2
 ufw allow 'Apache'
+
+region=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+echo "<h2> Это работает в регионе $region </h2><br>Build by psybrat" > /var/www/html/index.html
