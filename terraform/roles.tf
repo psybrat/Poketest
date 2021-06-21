8da62cc19ba4b6da659e8052c05fb2150a248e4e
psybrat <psybrat+git@ya.ru>
2021-06-21 23:26:29 +0300

    Terraform HighlyAvailable server config v.8.1 Add roles for CodeDeploy

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -4,11 +4,24 @@ locals {
 
 
 resource "aws_iam_role" "code_deploy_instance" {
-  name               = "pokemon_CodeDeployInstance_role"
-  assume_role_policy = "${file("${AMAZON_ROLES_DIR}/AmazonEC2RoleforAWSCodeDeploy.json")}"
+  name               = "instance_role"
+  assume_role_policy = file("${AMAZON_ROLES_DIR}/ec2_role.json")
 }
 
+resource "aws_iam_role_policy" "code_deploy_instance_policy" {
+  name = "instance_role_policy"
+  policy = file("${AMAZON_ROLES_DIR}/AmazonEC2RoleforAWSCodeDeploy.json")
+  role = aws_iam_role.code_deploy_instance.id
+}
+
+
 resource "aws_iam_role" "code_deploy" {
-  name               = "pokemon_CodeDeploy_role"
-  assume_role_policy = "${file("${AMAZON_ROLES_DIR}/AWSCodeDeployRole.json")}"
+  name               = "code_deploy_role"
+  assume_role_policy = file("${AMAZON_ROLES_DIR}/codedeploy_role.json")
+}
+
+resource "aws_iam_role_policy" "code_deploy_policy" {
+  name = "codedeploy_role_policy"
+  policy = file("${AMAZON_ROLES_DIR}/AWSCodeDeployRole.json")
+  role = aws_iam_role.code_deploy.id
 }
