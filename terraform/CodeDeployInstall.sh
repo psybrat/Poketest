f56dba561996c652848e6f9d053263f64e8e449f
psybrat <psybrat+git@ya.ru>
2021-06-23 01:56:22 +0300

    Terraform HighlyAvailable server config v.10.2 Github Actions added

--- terraform/CodeDeployInstall.sh
+++ terraform/CodeDeployInstall.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 apt update
-apt install ruby-full
-apt install wget
 
-echo -----------Getting Region ID---------------------
-REGION_ID=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone-id)
+echo "-------------Install Apache----------------------"
+apt install -y apache2
+ufw allow 'Apache'
+echo "-------------Creating test page------------------"
+region=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+echo "<h2><center> This server at region $region </center> </h2><br>Build by psybrat" > /var/www/html/index.html
+
+echo "------------Install Ruby-------------------------"
+apt install -y ruby-full
+
+echo "------------Install wget-------------------------"
+apt install -y wget
+
+echo "-----------Getting Region ID---------------------"
+REGION_ID=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | \
+            python3 -c "import sys, json; print(json.load(sys.stdin)['region'])")
 
 echo "----------Downloading CodeDeploy kit-------------"
 cd /home/ubuntu
