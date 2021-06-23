effa045acc43efbda79c219c32df92cf9be74d89
psybrat <psybrat+git@ya.ru>
2021-06-23 17:12:55 +0300

    Terraform HighlyAvailable server config v.10.4 Github Actions added

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -8,10 +8,11 @@ resource "aws_iam_role" "code_deploy_instance" {
   assume_role_policy = file("${local.AMAZON_ROLES_DIR}/ec2_role.json")
 }
 
-resource "aws_iam_role_policy" "code_deploy_instance_policy" {
+resource "aws_iam_role_policy_attachment" "code_deploy_instance" {
   name = "instance_role_policy"
-  policy = file("${local.AMAZON_ROLES_DIR}/AmazonEC2RoleforAWSCodeDeploy.json")
-  role = aws_iam_role.code_deploy_instance.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+  #  policy = file("${local.AMAZON_ROLES_DIR}/AmazonEC2RoleforAWSCodeDeploy.json")
+  role = aws_iam_role.code_deploy_instance.name
 }
 
 
@@ -20,8 +21,9 @@ resource "aws_iam_role" "code_deploy" {
   assume_role_policy = file("${local.AMAZON_ROLES_DIR}/codedeploy_role.json")
 }
 
-resource "aws_iam_role_policy" "code_deploy_policy" {
+resource "aws_iam_role_policy_attachment" "code_deploy_policy" {
   name = "codedeploy_role_policy"
-  policy = file("${local.AMAZON_ROLES_DIR}/AWSCodeDeployRole.json")
+#  policy = file("${local.AMAZON_ROLES_DIR}/AWSCodeDeployRole.json")
   role = aws_iam_role.code_deploy.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
