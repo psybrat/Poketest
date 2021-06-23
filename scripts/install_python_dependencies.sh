d062c7e0e7e77fc179ec4caeb4aee3f68432e965
psybrat <psybrat+git@ya.ru>
2021-06-23 19:23:44 +0300

    Terraform HighlyAvailable server config v.10.9 Github Actions added

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -5,4 +5,4 @@ chown ubuntu:ubuntu /home/ubuntu/www/pokemon-venv
 chown ubuntu:ubuntu /home/ubuntu/www/pokemon-venv/*
 source /home/ubuntu/www/pokemon-venv/bin/activate
 
-pip install -r /home/ubuntu/www/pokemon/requirmenets.txt
+pip install -r /home/ubuntu/www/pokemon/requirements.txt
