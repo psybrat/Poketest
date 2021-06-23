c600fc64cf91bb963e8d077a50a199b04763752e
psybrat <psybrat+git@ya.ru>
2021-06-23 17:14:28 +0300

    Terraform HighlyAvailable server config v.10.5 Github Actions added

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role" "code_deploy_instance" {
 }
 
 resource "aws_iam_role_policy_attachment" "code_deploy_instance" {
-  name = "instance_role_policy"
+#  name = "instance_role_policy"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
   #  policy = file("${local.AMAZON_ROLES_DIR}/AmazonEC2RoleforAWSCodeDeploy.json")
   role = aws_iam_role.code_deploy_instance.name
@@ -22,7 +22,7 @@ resource "aws_iam_role" "code_deploy" {
 }
 
 resource "aws_iam_role_policy_attachment" "code_deploy_policy" {
-  name = "codedeploy_role_policy"
+#  name = "codedeploy_role_policy"
 #  policy = file("${local.AMAZON_ROLES_DIR}/AWSCodeDeployRole.json")
   role = aws_iam_role.code_deploy.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
