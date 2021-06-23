13d076e150cadc3d81272bfb3213adc20f658345
psybrat <psybrat+git@ya.ru>
2021-06-23 20:03:29 +0300

    Terraform HighlyAvailable server config v.10.11 Github Actions added

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/www/pokemon/
 source /home/ubuntu/www/pokemon-venv/bin/activate
-python manage.py runserver 0.0.0.0:8000
+screen -d -m python manage.py runserver 0.0.0.0:8000
