36f35e0a1e897d49d09f60afd02cd230b5e0170e
psybrat <psybrat+git@ya.ru>
2021-06-27 16:22:15 +0300

    Refactor structure fix

--- IaC/scripts/start_application.sh
+++ IaC/scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/www/pokemon/
+cd /home/ubuntu/www/pokemon/app/
 source /home/ubuntu/www/pokemon-venv/bin/activate
 screen -d -m python manage.py runserver 0.0.0.0:8000
