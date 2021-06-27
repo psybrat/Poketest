166ec573481c72598665e239b257ac4290d42fba
psybrat <psybrat+git@ya.ru>
2021-06-27 18:00:34 +0300

    Add starting server with service unit fix

--- IaC/scripts/add_app_service.sh
+++ IaC/scripts/add_app_service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cp /home/ubuntu/www/pokemon/IaC/pokemon_app.service /usr/lib/systemd/user
+cp /home/ubuntu/www/pokemon/IaC/pokemon_app.service /usr/lib/systemd/user/
 systemctl --user daemon-reload
